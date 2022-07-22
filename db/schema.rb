a59f8a06a50d11b873f426dd31fecb885f2e7f8a
yyrnutdkg <yyrnutdkg@gmail.com>
2022-07-23 00:23:29 +0900

    [add] notificationテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_151818) do
+ActiveRecord::Schema.define(version: 2022_07_22_152205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2022_07_22_151818) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["place_id"], name: "index_events_on_place_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.text "comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "places", force: :cascade do |t|
