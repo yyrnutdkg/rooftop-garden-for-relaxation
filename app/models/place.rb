4ccf79530a48cf95cdfd4250324d9448315704ef
yyrnutdkg <yyrnutdkg@gmail.com>
2022-07-24 23:36:18 +0900

    [add]place_tag rspecの作成

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
   has_many :congestions
   has_many :events
+  has_many :place_tags
+  has_many :tags, through: :place_tags
   has_many :stores
 
   validates :name, presence: true
