7f96e10ccd8ff121f9ca4739ab1fa56f306c7ef2
yyrnutdkg <yyrnutdkg@gmail.com>
2022-08-15 20:20:04 +0900

    [add]placeテーブルに緯度経度管理のlonlatを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_145259) do
+ActiveRecord::Schema.define(version: 2022_08_15_100540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_08_12_145259) do
     t.text "map_link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
   end
 
   create_table "stores", force: :cascade do |t|
