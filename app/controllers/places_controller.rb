d370217021db4db0c7565cf5ee0fdc281f2e015a
yyrnutdkg <yyrnutdkg@gmail.com>
2022-08-23 00:34:11 +0900

    [add] paginateの実装

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   def index
     @search_places_form = SearchPlacesForm.new(search_params)
-    @places = @search_places_form.search.order(id: :desc)
+    @places = @search_places_form.search.order(id: :desc).page(params[:page])
   end
 
   def show; end
