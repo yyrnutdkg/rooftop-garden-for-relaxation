1f90dab8699bae9ef0b2dfa8d0b4378ee7a0f265
yyrnutdkg <yyrnutdkg@gmail.com>
2022-08-25 23:39:17 +0900

    [add]yarnでのbootstrap設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import '../stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 //jquery追加
-require('jquery')
-
+require('jquery');
 import "./current-location-search"
