734fb0e7c6edff90a0c3d2c7ce5952681611e788
yyrnutdkg <yyrnutdkg@gmail.com>
2022-09-05 05:33:48 +0900

    [fix]eventテーブルのstart_time,end_timeのカラム削除

--- db/migrate/20220904200856_change_data_type_start_time_and_end_time_of_events.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTypeStartTimeAndEndTimeOfEvents < ActiveRecord::Migration[6.1]
-  def change
-    change_column :events, :start_time, :timestamp
-    change_column :events, :end_time, :timestamp
-  end
-end
