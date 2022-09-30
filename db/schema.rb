3f18d959f8dd8eb87d261a09bb2bec9e09074ee6
yyrnutdkg <yyrnutdkg@gmail.com>
2022-10-01 01:37:51 +0900

    [add]migration file の適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_150359) do
+ActiveRecord::Schema.define(version: 2022_09_30_163458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 2022_09_30_150359) do
     t.string "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "user_role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
