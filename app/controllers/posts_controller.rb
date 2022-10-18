5908d1a9cdcc297534f44033ea4e58805640405b
yyrnutdkg <yyrnutdkg@gmail.com>
2022-10-18 23:59:26 +0900

    [add]postのcreate作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
   def create
+    @comment = current_user.posts.build(post_params)
+    @comment.save
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
