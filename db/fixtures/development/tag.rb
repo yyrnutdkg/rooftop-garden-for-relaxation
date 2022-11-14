82ff302ad13c707f257b918307fe79315fd85067
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-15 02:08:11 +0900

    [add]tag seedの分割

---
+++ db/fixtures/development/tag.rb
@@ -0,0 +1,14 @@
+Tag.seed do |tag|
+  tag.id = 1
+  tag.name = "東京"
+end
+
+Tag.seed do |tag|
+  tag.id = 1
+  tag.name = "東京23区内"
+end
+
+Tag.seed do |tag|
+  tag.id = 1
+  tag.name = "東京23区外"
+end
