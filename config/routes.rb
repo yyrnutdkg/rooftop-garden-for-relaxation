5e90f04adead3d59861b68600ac3cb9fcc73406a
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-25 23:34:34 +0900

    [fix]postのrouteを追加と修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'static_pages#top'
   get 'search_detail', to: 'static_pages#detail'
+  get 'about', to: 'static_pages#about'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'tos', to: 'static_pages#tos'
+  get 'info', to: 'static_pages#info'
 
   resources :users, only: %i[new create]
   get '/login', to: 'user_sessions#new'
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'user_sessions#destroy'
 
   resources :places, only: %i[index show] do
-    resources :posts, only: %i[create destroy]
+    resources :posts, only: %i[create edit update destroy]
   end
   resources :bookmarks, only: %i[create destroy]
   resources :static_pages, only: %i[show]
