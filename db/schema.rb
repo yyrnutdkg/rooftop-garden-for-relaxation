8595df8ab186d54b101fe4c32d73101bed9773ae
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-26 00:28:13 +0900

    [add]migration fileの反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_25_124803) do
+ActiveRecord::Schema.define(version: 2022_11_25_152641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2022_11_25_124803) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["place_id"], name: "index_congestions_on_place_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.index ["email"], name: "index_contacts_on_email", unique: true
   end
 
   create_table "events", force: :cascade do |t|
