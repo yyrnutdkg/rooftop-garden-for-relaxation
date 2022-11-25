1542d16a1726c7985bbb1b8479a4e60692daa8bd
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-25 19:34:18 +0900

    [add]投稿の編集、更新機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+    @place = Place.find_by(id: @post.place_id)
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if(@post.update(post_params))
+      redirect_to place_path(params[:place_id]), success: t('defaults.message.updated', item: Post.model_name.human )
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: Post.model_name.human )
+      render :edit
+    end
+  end
+
   def destroy
     @post = current_user.posts.find_by(id: params[:id])
     current_user.delete_post(@post)
@@ -22,6 +37,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description).merge(place_id: params[:place_id])
+    params.require(:post).permit(:title, :description, :post_type).merge(place_id: params[:place_id])
   end
 end
