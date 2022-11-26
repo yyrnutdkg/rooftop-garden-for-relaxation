8b9f6583de1e3a44f24fbf427dccd77cbe8a6508
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-27 02:39:06 +0900

    [fix]uploaderの修正, viewの修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -21,7 +21,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.development?
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
