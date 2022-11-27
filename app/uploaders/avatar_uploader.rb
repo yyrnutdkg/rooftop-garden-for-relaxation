051ecc4b3454c09042b38032fba3d03c579d840e
yyrnutdkg <yyrnutdkg@gmail.com>
2022-11-27 17:49:34 +0900

    [fix]画像upload方法の変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,29 +1,23 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [300, 200]
+  process resize_to_limit: [200, 200]
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  if Rails.env.development?
-    storage :file
-  else
-    include Cloudinary::CarrierWave
-    process :convert => 'png'
-    process :tags => ['icon']
-  end
-
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
 
   def store_dir
-    if Rails.env.development?
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
